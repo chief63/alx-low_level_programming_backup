4219eca049069ef467d57fcc919ca66c4d980b81
chief63 <francislameck01@gmail.com>
2023-06-19 09:36:36 -0700

    gui

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,6 @@
-#!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -o program program.c
+#include <stdio.h>
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
